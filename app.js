f53e435e789b4afbb65220fbb7e676dff4f947f9
alveis <michaelngiri@gmail.com>
2018-08-27 14:15:53 +0100

    starting code for the project

--- app.js
+++ app.js
@@ -3,16 +3,55 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
-	res.send("hello world");
+	res.send("hi, this is StackOverflow Lite");
 });
 
-app.get('/hi', function (req, res) {
+//Register a user 
+app.post('/auth/signup ', function (req, res) {
+   // res.send("hi there");
+});
+
+//Login a user 
+app.post('/auth/login', function (req, res) {
     res.send("hi there");
 });
 
-app.get('/foo/baa', function (req, res) {
+//Fetch all questions 
+app.get('/questions', function (req, res) {
+	res.send("StackOverflow Lite");
+});
+
+//Fetch a specific  question 
+//This should come with all the  answers  provided so far for the question. 
+app.get('/questions/<questionId>', function (req, res) {
+	res.send("StackOverflow Lite");
+});
+
+//Post a question 
+app.post('/questions', function (req, res) {
     res.send("hi there");
 });
+
+//Delete a question
+//This endpoint should be available to  the author of the question. 
+app.delete('/questions/<questionId>', function (req, res) {
+	res.send("StackOverflow Lite");
+});
+
+//Post an answer to  a question
+app.post('/questions/<questionId>/answers', function (req, res) {
+    res.send("hi there");
+});
+
+//Mark an answer as  accepted or  update an answer.
+//This endpoint should be available to  only the answer author and question 
+// author. The answer author calls the  route to
+// update answer while the  question author calls the route to  accept answer.
+app.put('/questions/<questionId>/answers/<answerId>', function (req, res) {
+    res.send("hi there");
+});
+
+
 
 //app.use(express.static(public));
 
