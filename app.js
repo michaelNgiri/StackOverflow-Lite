c0a4c81a476e961f04c6dd657f2cc1f681546ae2
alveis <michaelngiri@gmail.com>
2018-08-27 11:32:58 +0100

    add all project dependencies

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 var express = require('express');
 var app = express();
-var port = 3000;
+var port = process.env.PORT || 3000;
+
+app.get('/', function (req, res) {
+	res.send("hello world");
+});
+
+app.get('/hi', function (req, res) {
+    res.send("hi there");
+});
+
+app.get('/foo/baa', function (req, res) {
+    res.send("hi there");
+});
+
+//app.use(express.static(public));
+
 app.listen(port, function(err){
 	console.log('server started at port: ' + port);
 });
