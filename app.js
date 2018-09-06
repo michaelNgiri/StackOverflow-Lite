5c713243f60412c5be9082db8d7b8793b8f57d16
alveis <michaelngiri@gmail.com>
2018-09-06 10:57:53 +0100

    users can upvote answers

--- app.js
+++ app.js
@@ -314,7 +314,27 @@ app.put('/questions/:questionId/answers/:answerId', verifyToken, (req, res)=>{
 
 });
 
-app.get('/upvote/:answerId', function(req, res) {
+app.get('/upvote/:answerId', (req, res)=>{
+    const userId = 1;// req.body.user_id;
+    const answerId = 1;//req.body.answer_id;
+    const time =  new Date().toLocaleString();
+
+        pool.query("INSERT INTO upvotes(user_id, answer_id, created_at) VALUES('"+userId+"', '"+answerId+"', '"+time+"'); ", [],function(err,result) {
+            if(err) {
+                console.log(err);
+                console.log('action failed');
+                res.status(400).json({
+                    status: 400,
+                    msg: "could not complete the requested action, try later"
+                });
+            }else {
+                console.log('action completed');
+                res.status(200).json({
+                    status: 200,
+                    msg: "succesful!"
+                });
+            }
+        });
 
 });
 
