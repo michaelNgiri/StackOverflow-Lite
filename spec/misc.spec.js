5735a3396082d48d889f12d200e0cacbb21d2d67
alveis <michaelngiri@gmail.com>
2018-09-06 16:42:52 +0100

    write miscellaneous tests

---
+++ spec/misc.spec.js
@@ -0,0 +1,29 @@
+const Request = require('request');
+
+describe("Upvoting of an answer", ()=>{
+    let data = {};
+    beforeAll((done)=>{
+        Request.post({url:'http://localhost:3000/upvote/2', form: {question_id:2}}, function(err,httpResponse,body){
+            console.log(httpResponse['statusCode']);
+            data.status = httpResponse['statusCode'];
+            done();
+        });
+    });
+    it('should return 400 if the request token is missing', ()=>{
+        expect(data.status).toBe(403);
+    });
+});
+
+describe("POST route", ()=>{
+    let data = {};
+    beforeAll((done)=>{
+        Request.post({url:'http://localhost:3000/upvote/2', form: {question_id:2}}, function(err,httpResponse,body){
+            console.log(httpResponse['statusCode']);
+            data.status = httpResponse['statusCode'];
+            done();
+        });
+    });
+    it('should return 404 if the request is sent as GET instead of POST', ()=>{
+        expect(data.status).toBe(403);
+    });
+});
