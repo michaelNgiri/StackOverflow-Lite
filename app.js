86ff4e7ae4953289b744c8a0ffca70b620e8d1fc
alveis <michaelngiri@gmail.com>
2018-09-07 13:46:29 +0100

    cleanup the working directory and remove unneccessary files

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const bcrypt = require('bcrypt');
-const totoro = require('totoro-node');
 //const path = require('path');
 
 const cookieParser = require('cookie-parser');
