7301b7b0f9dff5a182ef14e017989ba7b2a1a64e
alveis <michaelngiri@gmail.com>
2018-09-08 13:58:49 +0100

    remove redundant lines of code

--- routes/auth.js
+++ routes/auth.js
@@ -24,6 +24,10 @@ router.post('/login', function (req, res) {
         pool.query("SELECT password FROM users WHERE email = '"+email+"' ", [], function (err, result) {
             console.log(result.rows.length);
             if (result.rows.length < 1) {
+                res.status(401).json({
+                            status: 401,
+                            msg: 'this email is not registered'
+                        });
                 console.log('this email is not registered');
 
             }else {
