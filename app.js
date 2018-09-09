c30ff0d7744c7cb2d329b5cfcf5285791d0fde6c
alveis <michaelngiri@gmail.com>
2018-09-09 23:55:29 +0100

    allow cors in backend

--- app.js
+++ app.js
@@ -21,9 +21,18 @@ const votesRoute = require('./src/votes');
 //configure body-parser for express
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
+app.use(function(req, res, next) {  
+      res.header('Access-Control-Allow-Origin', req.headers.origin);
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      res.header("Access-Control-Allow-Headers","*");
+      res.header('Access-Control-Allow-Credentials', true);
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+      next();
+ });
 app.use('/api/v1/auth', authRoute);
 app.use('/api/v1/questions', questionsRoute);
 app.use('/api/v1/votes', votesRoute);
+ 
 
 
  /*
@@ -33,7 +42,10 @@ app.use('/api/v1/votes', votesRoute);
 */
 app.get('/', (req, res)=>{
     res.setHeader('Content-Type', 'application/json');
-    res.send('welcome to stackoverflow-lite');
+    res.status(200).json({
+        status:200,
+        msg:'welcome to stackoverflow-lite'
+    });
 });
 
  /*
@@ -41,8 +53,10 @@ app.get('/', (req, res)=>{
  * description: "The API base url or home page"
 */
 app.get('/api/v1/', (req, res)=>{
-    res.setHeader('Content-Type', 'application/json');
-    res.send('welcome to stackoverflow-lite');
+    res.status(400).json({
+        status:400,
+        msg:'welcome to stackoverflow-lite'
+    });
 });
 
 
@@ -51,7 +65,7 @@ app.get('/api/v1/', (req, res)=>{
  * @oas [get] /api/v1/index.html
  * description: "The API base url or home page"
 */
-app.get('/api/v1/index.html', (req, res)=>{
+app.get('/', (req, res)=>{
         res.sendFile(__dirname + "/" + "index.html");
     });
 
