544cd4462794212dfc4c5896e5beb45bc9c35f6d
Armando Bracho <armandombi@gmail.com>
2018-10-03 09:43:12 +0100

    Update verifyUserInfo.js
    Adding function description for userInfoIsValid

--- middlewares/verifyUserInfo.js
+++ middlewares/verifyUserInfo.js
@@ -1,3 +1,8 @@
+ /// <summary>
+ /// Verifies that the user information is in a valid format and not empty
+ /// </summary>
+ /// <param name="user"></param>
+ /// <returns>true for a valid user and false for an invallid user</returns>
 function userInfoIsValid(user){
     if (typeof user.email === "string" &&
         user.email.trim() !== '' &&
