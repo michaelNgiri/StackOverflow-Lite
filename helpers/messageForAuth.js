2bd88241b7df2c087721d5aa584bd584d3e5144f
Kartik Pandey <kartikpandey2@gmail.com>
2018-10-03 19:06:06 +0530

    refactor: imporove code readability and added error handling

---
+++ helpers/messageForAuth.js
@@ -0,0 +1,10 @@
+module.exports = {
+	emailNotRegistered: "this email is not registered",
+	loginSuccess: "success, you are logged in",
+	invalidCredentials: "invalid email or password",
+	incorrectInfo: "the info you submitted does not seem to be right",
+	errLogin: "Temporarily out of service, try logging in again later",
+	registrationSuccess: 'registration successful',
+	emailUsed: 'email in use',
+	userNotExist: 'user does not exist'
+}
