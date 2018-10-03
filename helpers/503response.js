44290a5c23b4718e163d3a32ce2bdcda28b21497
alveis <michaelngiri@gmail.com>
2018-10-03 09:07:27 +0100

    additional documentation for 404and503 helpers

--- helpers/503response.js
+++ helpers/503response.js
@@ -1,3 +1,5 @@
+//send a http status:503 and a descriptive messages to the client
+//called when the api host is out of service
 function send503(res, msg) {
     res.status(503).json({
         status: 503,
