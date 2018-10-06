2280e41abae14679d3eb580f1846daed4178981e
Maciej Bojko <maciej.bojko@gmail.com>
2018-10-06 21:17:26 +0200

    helper functions refactor

--- helpers/503response.js
+++ helpers/503response.js
@@ -1,10 +1,10 @@
 //send a http status:503 and a descriptive messages to the client
 //called when the api host is out of service
+
+const send = require('./responseJSON');
+
 function send503(res, msg) {
-    res.status(503).json({
-        status: 503,
-        message: msg
-    });
+    return send(res, 503, msg);
 }
 
 module.exports=send503;
